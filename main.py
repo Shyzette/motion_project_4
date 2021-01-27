8840c94aaa9f8e8888e58684c62f8d675a6d3694
Shyzette <72762164+Shyzette@users.noreply.github.com>
2021-01-27 11:00:20 -0500

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,3 @@
+while True:
+    if input.acceleration(Dimension.X) > 10:
+        light.set_all(light.rgb(0,255,255))
---
+++ main.ts
@@ -0,0 +1,6 @@
+while (true) {
+    if (input.acceleration(Dimension.X) > 10) {
+        light.setAll(light.rgb(0, 255, 255))
+    }
+    
+}
